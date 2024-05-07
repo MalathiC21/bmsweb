1cc4385dea70b2e7ebc2c0e421e7afb1c54771e8
MalathiC21 <malathichandran21@gmail.com>
2024-05-07 12:28:47 +0530

    bmsweb

---
+++ js/movies.js
@@ -0,0 +1,202 @@
+ const moviesData = [
+    {
+        id:1,
+        movieName:"rathnam",
+        img:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC8xMCAgMTQuN0sgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00384801-txsguleyam-portrait.jpg",
+        bgimg:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/rathnam-et00384801-1713951141.jpg",
+        rating:"4.5/10",
+        vote:"(14.8 votes)",
+        language:["Tamil", "Telugu"],
+        duration:"2h 36m",
+        type:"Action, Drama",
+        certificate:"UA",
+        released:"26 Apr 2024",
+        cast:[
+            {
+                name:"Vishal",
+                img:"https://in.bmscdn.com/iedb/artist/images/website/poster/large/vishal-krishna-reddy-16275-24-08-2021-06-47-02.jpg",
+            },
+            {
+                name:"Priya",
+                img:"https://in.bmscdn.com/iedb/artist/images/website/poster/large/priya-bhavani-shankar-1086843-16-10-2017-01-49-49.jpg",
+            },
+            {
+                name:"Samuthirakani",
+                img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/samuthirakani-1052542-24-03-2017-17-46-22.jpg",
+            },
+            {
+                name:"Yogi Babu",
+                img:"https://in.bmscdn.com/iedb/artist/images/website/poster/large/yogi-babu-1055754-02-12-2021-12-53-23.jpg",
+            },
+            {
+                name:"Gautham",
+                img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/gautham-menon-2889-08-02-2022-03-33-26.jpg",
+            }
+        ],
+        director:"Hari",
+        musicDirector:"Devi Sri Prasad",
+        screenType:"2D",
+        releaseDate:"22-04-2024",
+        aboutMovie:"Rathnam works as a henchman under MLA Panner Selvam. A girl travels to Vellore for an interview and a few rowdies try to kill her but Rathnam saves her and becomes her guardian angel. How long can he protect her?",
+        
+    },
+    {
+        id:2,
+        movieName:"aavesham",
+        img:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OS4yLzEwICAxMDguMUsgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00386326-geatkxcgfb-portrait.jpg",
+        bgimg:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/aavesham-et00386326-1707203790.jpg",
+        rating:"9.2/10",
+        vote:"(14.8 votes)",
+        language:["Malayalam"],
+        duration:"2h 38m",
+        type:"Action, Comedy",
+        certificate:"UA",
+        released:"11 Apr 2024",
+        aboutMovie:"The story revolves around the Life of Aalparambil Gopi and his dear friend Malgosh. K. P. and the little surprises that life has in store for them.",
+        cast:[ {
+            name:"Fahad",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/fahadh-faasil-37756-1661702033.jpg",
+        },
+        {
+            name:"Priya",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/pooja-mohanraj-2034272-1707549835.jpg",
+        },
+        {
+            name:"Mansoor",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/mansoor-ali-khan-35044-24-03-2017-15-38-35.jpg",
+        },
+        {
+            name:"Aashish",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/ashish-vidyarthi-3400-27-08-2020-02-59-37.jpg",
+        },
+        {
+            name:"Sajin",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/sajin-gopu-2022805-1665496386.jpg",
+        }
+        ],
+        director:"Jithu Madhavan",
+        musicDirector:"Sushin Shyam",
+        screenType:"2D",
+        releaseDate:"22-04-2024",
+       
+    },
+    {   
+        id:3,
+        movieName:"godzilla-x-kong",
+        img:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC43LzEwICAxMTEuMUsgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00358147-lvaczsfxxw-portrait.jpg",
+        bgimg:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/godzilla-x-kong-the-new-empire-et00358147-1714480722.jpg",
+        rating:"6/10",
+        vote:"(14.8 votes)",
+        language:["English","Tamil"],
+        duration:"2h 05m",
+        type:"Action, Sci-fi",
+        certificate:"UA",
+        released:"24 Apr 2024",
+        aboutMovie:"An all-new adventure that pits the almighty Kong and the fearsome Godzilla against a colossal undiscovered threat hidden within our world, challenging their very existence-and our own.",
+        cast:[ {
+            name:"Rebecca",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/rebecca-hall-7076-11-09-2017-05-49-56.jpg",
+        },
+        {
+            name:"Brian",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/brian-tyree-henry-1096515-26-10-2018-14-45-47.jpg",
+        },
+        {
+            name:"Dan Steven",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/dan-stevens-38327-24-03-2017-12-33-39.jpg",
+        },
+        {
+            name:"Rachael",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/rachel-house-1072845-24-03-2017-13-17-01.jpg",
+        },
+        {
+            name:"Kaylee",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/kaylee-hottle-2034219-1707383907.jpg",
+        }],
+        director:"Adam",
+        musicDirector:"Tom",
+        screenType:"3D",
+        releaseDate:"22-04-2024",
+        
+    },
+    {   
+        id:4,
+        movieName:"manjummal boys",
+        img:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-MTkuOEsgTGlrZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00385404-lqnnfcvasm-portrait.jpg",
+        bgimg:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/manjummel-boys-et00386670-1707452965.jpg",
+        rating:"9/10",
+        vote:"(14.8 votes)",
+        language:["Tamil", "Telugu","Kannada"],
+        duration:"2h 30m",
+        type:"Action, Sport",
+        certificate:"UA",
+        released:"24 Apr 2024",
+        aboutMovie:"The story revolves around the Life of Aalparambil Gopi and his dear friend Malgosh. K. P. and the little surprises that life has in store for them.",
+        cast:[ {
+            name:"Nivin",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/nivin-pauly-30511-1657870897.jpg",
+        },
+        {
+            name:"Dhyan",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/dhyan-sreenivasan-40530-1678248547.jpg",
+        },
+        {
+            name:"Manju",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/manju-pillai-3993-1700138908.jpg",
+        },
+        {
+            name:"Salim",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/salim-kumar-1987-24-03-2017-12-54-24.jpg",
+        },
+        {
+            name:"Gautham",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/shine-tom-chacko-40241-1683013135.jpg",
+        }],
+        director:"Dharani",
+        musicDirector:"Vidyasagar",
+        screenType:"2D",
+        releaseDate:"22-04-2024",
+        
+    },
+    {   
+        id:5,
+        img:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-MjAuN0sgTGlrZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00375399-qhmpyhmesk-portrait.jpg",
+        bgimg:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/the-fall-guy-et00375399-1712039103.jpg",
+        movieName:"fall-guy",
+        rating:"5.5/10",
+        vote:"(14.8 votes)",
+        language:["English","Tamil"],
+        duration:"2h 46m",
+        type:"Adventure, Sci-fi",
+        certificate:"UA",
+        released:"24 Apr 2024",
+        aboutMovie:"He`s a stuntman, and like everyone in the stunt community, he gets blown up, shot, crashed, thrown through windows and dropped from the highest of heights, all for our entertainment.",
+        cast:[ {
+            name:"Ryan",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/ryan-gosling-25971-24-03-2017-17-59-22.jpg",
+        },
+        {
+            name:"Emily",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/emily-blunt-4134-24-03-2017-12-41-22.jpg",
+        },
+        {
+            name:"Winston",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/winston-duke-1266835-27-03-2019-05-47-48.jpg",
+        },
+        {
+            name:"Aaron Taylor",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/aaron-taylor-johnson-7092-24-03-2017-17-33-08.jpg",
+        },
+        {
+            name:"Stephanie",
+            img:"https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/stephanie-hsu-2021290-1660206198.jpg",
+        }],
+        director:"Denis",
+        musicDirector:"Dave",
+        screenType:"3D",
+        releaseDate:"22-04-2024",
+        
+    }
+   
+]
+
---
+++ js/theatres.js
@@ -0,0 +1,455 @@
+ const theatresData = [
+    {
+        id:11,
+        theatreName:"AGS",
+        location:"Chennai OMR",
+        movieList:["rathnam", "aavesham", "godzilla-x-kong"],
+        screens:[
+            {
+                id:121,
+                screenNo:"screen-1",
+                movieName:"rathnam",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            {
+                id:122,
+                screenNo:"screen-2",
+                movieName:"godzilla-x-kong",
+                price:350,
+                screenType:"3D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            {
+                id:123,
+                screenNo:"screen-3",
+                movieName:"aavesham",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            }
+        ]
+
+    },
+
+
+    {
+        id:12,
+        theatreName:"PVR",
+        location:"Chennai OMR",
+        movieList:["rathnam", "gilly", "godzilla-x-kong"],
+        screens:[
+            {
+                id:221,
+                screenNo:"screen-1",
+                movieName:"rathnam",
+                price:350,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            {
+                id:222,
+                screenNo:"screen-2",
+                movieName:"gilly",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            {
+                id:223,
+                screenNo:"screen-3",
+                movieName:"godzilla-x-kong",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+        ]
+
+    },
+
+    {
+        id:13,
+        theatreName:"National",
+        location:"Chennai Tambaram",
+        movieList:["gilly", "aavesham"],
+        screens:[
+            {
+                id:321,
+                screenNo:"screen-1",
+                movieName:"gilly",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            {
+                id:322,
+                screenNo:"screen-2",
+                movieName:"aavesham",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"03:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:15 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            
+        ]
+
+    },
+    {
+        id:14,
+        theatreName:"TPV multiplex",
+        location:"Chennai Egmore",
+        movieList:["rathnam", "manjummal boys","fall-guy"],
+        screens:[
+            {
+                id:421,
+                screenNo:"screen-1",
+                movieName:"manjummal boys",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            {
+                id:422,
+                screenNo:"screen-2",
+                movieName:"fall-guy",
+                price:350,
+                screenType:"3D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            {
+                id:422,
+                screenNo:"screen-3",
+                movieName:"rathnam",
+                price:350,
+                screenType:"3D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+        ]
+
+    },
+    {
+        id:15,
+        theatreName:"PSS multiplex",
+        location:"Chennai Tnagar",
+        movieList:["manjummal boys", "fall-guy"],
+        screens:[
+            {
+                id:521,
+                screenNo:"screen-1",
+                movieName:"manjummal boys",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            {
+                id:522,
+                screenNo:"screen-2",
+                movieName:"fall-guy",
+                price:250,
+                screenType:"2D",
+                visuals:"4K Dolby Atmos",
+                showData:[
+                    {
+                        id:1,
+                        showTime:"11:30 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:2,
+                        showTime:"02:45 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    },
+                    {
+                        id:3,
+                        showTime:"06:00 PM",
+                        seats:50,
+                        bookedSeats:[],
+                        seatAvailable:null
+                    }
+                ]
+            },
+            
+        ]
+
+    },
+]
